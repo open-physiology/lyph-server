df34f05c9647f8366a6f5d855d5ff66059d49529
mhelvens <mhelvens@gmail.com>
2015-08-14 12:33:07 +0200

    Running app through babel with webpack.

--- src/index.es6.js
+++ src/index.es6.js
@@ -1,48 +1,47 @@
 // imports
-var express = require('express');
-var bodyParser = require('body-parser');
+import express    from 'express';
+import bodyParser from 'body-parser';
 
 // the express application
-var app = express();
+let app = express();
 
 // middleware
 app.use(bodyParser.json());
 
 // to implement an API for a specific type of entity (e.g., lyphs)
 function implementEntity(name) {
-	app.get('/' + name, (req, res) => {
-		res.send("You requested the " + name + " collection.");
+	const jsonBody = req => JSON.stringify(req.body, null, '    ');
+	app.get(`/${name}`, (req, res) => {
+		res.send(`You requested the ${name} collection.`);
 	});
-	app.get('/' + name + '/:id', (req, res) => {
-		res.send("You requested " + name + " '" + req.params.id + "'");
+	app.get(`/${name}/:id`, (req, res) => {
+		res.send(`You requested ${name} '${req.params.id}'.`);
 	});
-	app.post('/' + name, (req, res) => {
-		res.send("You created a new " + name + " with fields \n" + JSON.stringify(req.body, null, '    '));
+	app.post(`/${name}`, (req, res) => {
+		res.send(`You created a new ${name} with fields \n ${jsonBody(req)}.`);
 	});
-	app.post('/' + name + '/:id', (req, res) => {
-		res.send("You modified " + name + " '" + req.params.id + "' with fields \n" + JSON.stringify(req.body, null, '    '));
+	app.post(`/${name}/:id`, (req, res) => {
+		res.send(`You modified ${name} '${req.params.id}' with fields \n ${jsonBody(req)}.`);
 	});
-	app.put('/' + name, (req, res) => {
-		res.send("You created a new " + name + " with full body \n" + JSON.stringify(req.body, null, '    '));
+	app.put(`/${name}`, (req, res) => {
+		res.send(`You created a new ${name} with full body \n ${jsonBody(req)}.`);
 	});
-	app.put('/' + name + '/:id', (req, res) => {
-		res.send("You replaced " + name + " '" + req.params.id + "' with full body \n" + JSON.stringify(req.body, null, '    '));
+	app.put(`/${name}/:id`, (req, res) => {
+		res.send(`You replaced ${name} '${req.params.id}' with full body \n ${jsonBody(req)}.`);
 	});
-	app.delete('/' + name + '/:id', (req, res) => {
-		res.send("You deleted " + name + " '" + req.params.id + "'");
+	app.delete(`/${name}/:id`, (req, res) => {
+		res.send(`You deleted ${name} '${req.params.id}'.`);
 	});
 }
-
 
 
 // Test stuff
 [
 	'lyphs',
 	'layers',
-	'materials',
 	'lyphTemplates',
 	'layerTemplates',
-	'materialTemplates',
+	'borders',
 	'nodes',
 	'processes',
 	'correlations',
@@ -55,4 +54,4 @@ function implementEntity(name) {
 
 
 // start listening on port 3000 (temporary)
-var server = app.listen(3000);
+let server = app.listen(3000);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+		.filter(function(x) {
+			return ['.bin'].indexOf(x) === -1;
+		})
+		.forEach(function(mod) {
+			nodeModules[mod] = 'commonjs ' + mod;
+		});
+
+module.exports = {
+	devtool: 'source-map',
+	entry: {
+		'index': ['./src/index.es6.js']
+	},
+	output: {
+		path: './dist',
+		filename: '[name].js',
+		sourceMapFilename: '[file].map'
+	},
+	target: 'node',
+	externals: nodeModules,
+	module: {
+		loaders: [
+			{ test: /\.es6\.js$/, loader: 'babel' }
+		]
+	}
+};
