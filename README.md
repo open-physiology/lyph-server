ac8ae072f94d55d7712f88286e663f7c95c004f3
mhelvens <mhelvens@gmail.com>
2015-08-14 10:53:21 +0200

    Minor README and .gitignore.

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Installation
+
+* Install MongoDB (http://www.mkyong.com/mongodb/mongodb-hello-world-example/)
+* Make sure 'mongod' is run at machine startup. Also, run it now.
+    - In Linux, a '@reboot' cron-job is used to auto-start daemons (https://en.wikipedia.org/wiki/Cron).
--- src/index.es6.js
+++ src/index.es6.js
@@ -9,35 +9,50 @@ var app = express();
 app.use(bodyParser.json());
 
 // to implement an API for a specific type of entity (e.g., lyphs)
-function implementAPI(entity) {
-	app.get('/' + entity, (req, res) => {
-		res.send("You requested the " + entity + " collection.");
+function implementEntity(name) {
+	app.get('/' + name, (req, res) => {
+		res.send("You requested the " + name + " collection.");
 	});
-	app.get('/' + entity + '/:id', (req, res) => {
-		res.send("You requested " + entity + " '" + req.params.id + "'");
+	app.get('/' + name + '/:id', (req, res) => {
+		res.send("You requested " + name + " '" + req.params.id + "'");
 	});
-	app.post('/' + entity, (req, res) => {
-		res.send("You created a new " + entity + " with fields \n" + JSON.stringify(req.body, null, '    '));
+	app.post('/' + name, (req, res) => {
+		res.send("You created a new " + name + " with fields \n" + JSON.stringify(req.body, null, '    '));
 	});
-	app.post('/' + entity + '/:id', (req, res) => {
-		res.send("You modified " + entity + " '" + req.params.id + "' with fields \n" + JSON.stringify(req.body, null, '    '));
+	app.post('/' + name + '/:id', (req, res) => {
+		res.send("You modified " + name + " '" + req.params.id + "' with fields \n" + JSON.stringify(req.body, null, '    '));
 	});
-	app.put('/' + entity, (req, res) => {
-		res.send("You created a new " + entity + " with full body \n" + JSON.stringify(req.body, null, '    '));
+	app.put('/' + name, (req, res) => {
+		res.send("You created a new " + name + " with full body \n" + JSON.stringify(req.body, null, '    '));
 	});
-	app.put('/' + entity + '/:id', (req, res) => {
-		res.send("You replaced " + entity + " '" + req.params.id + "' with full body \n" + JSON.stringify(req.body, null, '    '));
+	app.put('/' + name + '/:id', (req, res) => {
+		res.send("You replaced " + name + " '" + req.params.id + "' with full body \n" + JSON.stringify(req.body, null, '    '));
 	});
-	app.delete('/' + entity + '/:id', (req, res) => {
-		res.send("You deleted " + entity + " '" + req.params.id + "'");
+	app.delete('/' + name + '/:id', (req, res) => {
+		res.send("You deleted " + name + " '" + req.params.id + "'");
 	});
 }
 
+
+
 // Test stuff
-implementAPI('lyphs');
-implementAPI('connections');
-implementAPI('templates');
-implementAPI('nodes');
+[
+	'lyphs',
+	'layers',
+	'materials',
+	'lyphTemplates',
+	'layerTemplates',
+	'materialTemplates',
+	'nodes',
+	'processes',
+	'correlations',
+	'publications',
+	'variables',
+	'clinicalIndices',
+	'locatedMeasures',
+	'bagsOfPathology'
+].forEach(implementEntity);
+
 
 // start listening on port 3000 (temporary)
 var server = app.listen(3000);
