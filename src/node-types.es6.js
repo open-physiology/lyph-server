9fa93250cd872647a679508c4a5b1b2b0f8fa3ce
mhelvens <mhelvens@gmail.com>
2015-08-18 13:46:48 +0200

    added the 'compartments' entity type

--- src/node-types.es6.js
+++ src/node-types.es6.js
@@ -1,117 +1,130 @@
 const TYPES = {
-	lyphs:             {
+	lyphs: {
 		singularKey: "lyph",
 		singular: "lyph",
-		plural:   "lyphs",
-		schema:   {
+		plural: "lyphs",
+		schema: {
 			properties: {
-				name:     { type: 'string', required: true },
-				species:  { type: 'string', required: true },
-				template: { type: 'integer', required: true, skipDB: true },
-				location: { type: 'integer', skipDB: true } // a layer, or nothing
+				name: {type: 'string', required: true},
+				species: {type: 'string', required: true},
+				template: {type: 'integer', required: true, skipDB: true},
+				location: {type: 'integer', skipDB: true} // a layer, or nothing
 			}
 		}
 	},
-	layers:            {
-		singular:   "layer",
-		plural:     "layers",
-		schema:     {
+	layers: {
+		singular: "layer",
+		plural: "layers",
+		schema: {
 			properties: {}
 		},
 		noRestCrud: true
 	},
-	lyphTemplates:     {
-		singular: "lyph template",
-		plural:   "lyph templates",
-		schema:   {
+	compartments: {
+		singular: "compartment",
+		plural: "compartments",
+		schema: {
 			properties: {
-				name: { type: 'string', required: true }
-			}
-		}
-	},
-	layerTemplates:    {
-		singular: "layer template",
-		plural:   "layer templates",
-		schema:   {
-			properties: {
-				name:         { type: 'string' },
-				thickness:    {
-					// array of [min, max]
-					type:     'array',
-					items:    { type: 'number' },
-					required: true
-				},
-				lyphTemplate: { type: 'integer', required: true, skipDB: true },
-				position:     { type: 'integer' }
-			}
-		}
-	},
-	nodes:             {
-		singular: "node",
-		plural:   "nodes",
-		schema:   {
-			properties: {
-				attachments: {
-					type:     'array',
-					items:    {
-						type:       'object',
-						properties: {
-							layer:  { type: 'integer', required: true },
-							border: { enum: ['plus', 'minus', 'inner', 'outer'], required: true }
-						}
-					},
-					minItems: 1,
-					required: true,
-					skipDB:   true
+				lyphs: {
+					type: 'array',
+					items: {type: 'integer'},
+					skipDB: true
 				}
 			}
 		}
 	},
-	correlations:      {
+	lyphTemplates: {
+		singular: "lyph template",
+		plural: "lyph templates",
+		schema: {
+			properties: {
+				name: {type: 'string', required: true}
+			}
+		}
+	},
+	layerTemplates: {
+		singular: "layer template",
+		plural: "layer templates",
+		schema: {
+			properties: {
+				name: {type: 'string'},
+				thickness: {
+					// array of [min, max]
+					type: 'array',
+					items: {type: 'number'},
+					required: true
+				},
+				lyphTemplate: {type: 'integer', required: true, skipDB: true},
+				position: {type: 'integer'}
+			}
+		}
+	},
+	nodes: {
+		singular: "node",
+		plural: "nodes",
+		schema: {
+			properties: {
+				attachments: {
+					type: 'array',
+					items: {
+						type: 'object',
+						properties: {
+							layer: {type: 'integer', required: true},
+							border: {enum: ['plus', 'minus', 'inner', 'outer'], required: true}
+						}
+					},
+					minItems: 1,
+					required: true,
+					skipDB: true
+				}
+			}
+		}
+	},
+	correlations: {
 		singular: "correlation",
-		plural:   "correlations",
-		schema:   {
+		plural: "correlations",
+		schema: {
 			properties: {
-				comment: { type: 'string' }
+				comment: {type: 'string'}
 			}
 		}
 	},
-	publications:      {
+	publications: {
 		singular: "publication",
-		plural:   "publications",
-		schema:   {
+		plural: "publications",
+		schema: {
 			properties: {
-				uri:   { type: 'string', required: true },
-				title: { type: 'string' }
+				uri: {type: 'string', required: true},
+				title: {type: 'string'}
 			}
 		}
 	},
-	clinicalIndices:    {
+	clinicalIndices: {
 		singular: "clinical index",
-		plural:   "clinical indices",
-		schema:   {
+		plural: "clinical indices",
+		schema: {
 			properties: {
-				index: { type: 'number', required: true },
-				label: { type: 'string' }
+				index: {type: 'number', required: true},
+				label: {type: 'string'}
 			}
 		}
 	},
-	locatedMeasures:   {
+	locatedMeasures: {
 		singular: "located measures",
-		plural:   "located measures",
-		schema:   {
+		plural: "located measures",
+		schema: {
 			properties: {
-				quality: { type: 'string', required: true }
+				quality: {type: 'string', required: true}
 			}
 		}
 	},
 	bagsOfPathologies: {
 		singular: "bag of pathologies",
-		plural:   "bags of pathologies",
-		schema:   {
+		plural: "bags of pathologies",
+		schema: {
 			properties: {
-				excludedProcesses:   { type: 'array', items: { type: 'number' }, uniqueItems: true },
-				introducedProcesses: { type: 'array', items: { type: 'number' }, uniqueItems: true }
+				excludedProcesses: {type: 'array', items: {type: 'number'}, uniqueItems: true},
+				introducedProcesses: {type: 'array', items: {type: 'number'}, uniqueItems: true}
 			}
 		}
 	}
